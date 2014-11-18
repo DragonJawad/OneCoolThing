18b098e3f5dd44c5d547526bdee7514a488ccae3
jawadmn <jawadmn@gmail.com>
2014-11-17 22:41:25 -0500

    Avernir-like font included now [ie, custom fonts enabled]

---
+++ app/src/main/java/edu/umich/engin/cm/onecoolthing/Util/CustomFontTextView.java
@@ -0,0 +1,44 @@
+package edu.umich.engin.cm.onecoolthing.Util;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import edu.umich.engin.cm.onecoolthing.R;
+
+/**
+ * Created by jawad on 17/11/14.
+ *
+ * Just like a TextView but allows for a custom font
+ */
+public class CustomFontTextView extends TextView {
+    public CustomFontTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+
+    }
+
+    public CustomFontTextView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs!=null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomFontTextView);
+            String fontName = a.getString(R.styleable.CustomFontTextView_fontName);
+            if (fontName!=null) {
+                Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/"+fontName);
+                setTypeface(myTypeface);
+            }
+            a.recycle();
+        }
+    }
+}
