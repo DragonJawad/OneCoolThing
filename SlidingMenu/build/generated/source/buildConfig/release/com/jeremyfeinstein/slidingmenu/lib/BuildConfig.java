011ecdc37d861038bf4739d2b3b74b7d073f467e
jawadmn <jawadmn@gmail.com>
2014-12-05 15:01:58 -0500

    Build updates

--- SlidingMenu/build/generated/source/buildConfig/release/com/jeremyfeinstein/slidingmenu/lib/BuildConfig.java
+++ SlidingMenu/build/generated/source/buildConfig/release/com/jeremyfeinstein/slidingmenu/lib/BuildConfig.java
@@ -5,7 +5,7 @@ package com.jeremyfeinstein.slidingmenu.lib;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.jeremyfeinstein.slidingmenu.lib";
+  public static final String APPLICATION_ID = "com.jeremyfeinstein.slidingmenu.lib";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
