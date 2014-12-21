8cbb7aeda2a28186a22c872521d2c2e2b789a7ff
jawadmn <jawadmn@gmail.com>
2014-12-21 16:49:58 -0500

    Increased One Cool Feed's ViewPager to retain 5 items in memory (up from 3)

--- app/src/main/java/edu/umich/engin/cm/onecoolthing/CoolThings/OneCoolFeedFrag.java
+++ app/src/main/java/edu/umich/engin/cm/onecoolthing/CoolThings/OneCoolFeedFrag.java
@@ -70,9 +70,9 @@ public class OneCoolFeedFrag extends Fragment implements ViewPager.OnPageChangeL
             // Make this fragment listen to the adapter's changes
             mViewPager.setOnPageChangeListener(this);
 
-            // Set the pager to retain 3 of the Cool Things at one time
+            // Set the pager to retain 5 of the Cool Things at one time
             // Otherwise, only saves one Cool Thing at a time- so have to reload every one
-            mViewPager.setOffscreenPageLimit(3);
+            mViewPager.setOffscreenPageLimit(5);
         }
     }
 
